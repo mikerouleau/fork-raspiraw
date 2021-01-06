64f5d3eaa036658f81a632a8c5494a095aa7b0af
Rouleau, Michael <mike.rouleau@gatech.edu>
2021-01-06 00:39:10 -0500

    BF: typo fix

--- raspiraw.c
+++ raspiraw.c
@@ -1495,7 +1495,7 @@ int main(int argc, char **argv)
 
 	//Wait for trigger
 	wiringPiSetup();
-	pinMode(TRIGGERPIN, INPUT); // TODO: make input
+	pinMode(TRIGGER_PIN, INPUT); // TODO: make input
 	printf("Waiting for trigger on GPIO %d", TRIGGER_PIN);
 
 	while (digitalRead(TRIGGER_PIN) != HIGH)
